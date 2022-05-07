614f1b3f4a3b299639eaca54c222d8487e8444e0
Siobhan Durcan <durcansiobhan@gmail.com>
2022-05-07 18:33:01 +0100

    changed how nans work, fixed states

--- src/main_script.py
+++ src/main_script.py
@@ -14,40 +14,40 @@ prisoners,subset3=load.import_prisoners()
 subset=subset3
 target='sentence_length'
 th=20
-stand=1
-norm=0
+stand=0
+norm=1
 test_size=0.2
-Model='logr'
+Model='mlp'
 
 
 #returns a dataframe to pass into dataset_processor.encode_and_scale()
 config=load.import_config()
-#print('Config type after load',type(config))
 
-#inittialise dataset_processor object
+#initialise dataset_processor object
 dataset_processor=dp.dataset_processor(subset,config,target="sentence_length",th=th)
 
 #calculate sentence length before doing the preprocessing
 dataset_processor.calc_target()
 
+#apply one hot encoding and scaling
 #can stop scaling of continuous variables by setting stand=0 and norm=0
 dataset_processor.encode_and_scale(stand=stand,norm=norm)
 
-#we can get the encoded dataset from the dataset processor
+#get the name of the target column the dataset processor
+#this is dynamic based on the threshold
 target=dataset_processor.target_name
 prepped_subset=dataset_processor.dataset_out
 
 #save prepped subset
-#fdir=r'C:\Users\siobh\OneDrive\Masters\Dissertation\us_prisonsers\output/processed_subset'
 fdir='C:/Users/siobh/OneDrive/Masters/Dissertation/us_prisonsers/output/processed_subset/'
 prefix='subset'
 utils.name_and_pickle(prepped_subset,fdir,prefix,ext='pkl')
 
-
+#dropped any columns containing information about the target
 X=prepped_subset.drop(labels=target,axis=1)
 y=copy.deepcopy(prepped_subset[target])
 
-#we need to split into test and training data
+#split into test and training data
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=test_size, random_state=25)
 
 #then we create the model
@@ -57,66 +57,6 @@ md=model.model(X_train, X_test, y_train, y_test,model=Model)
 md.train()
 md.predict()
 md.get_scores()
-
-
-###OPTIONS
-#model
-#stand
-#norm
-#ytype binary,kr
-#test_train_split
-#loop through different subsets?
-#loop through options
-
-
-
-
-
-'''
-norms=[0,1]
-stands=[0,1]
-class_counts=[2,5,20]
-train_sizes=[0.1,0.2,0.3]
-model_list=['auto_class','mlp','lg']
-
-#mlp params
-activations=['identity’, ‘logistic’, ‘tanh’, ‘relu']
-solvers=['lbfgs’,‘sgd’, ‘adam']
-alphas=[]
-learning_rates=['constant’, ‘invscaling’, ‘adaptive']
-
-
-#mlp configurations
-#curated bin edges
-
-results={}
-
-count=0
-print(count)
-for subset in subsets:
-    for stand in stands:
-        for norm in norms:
-            for class_counts in class_counts:
-                for train_size in train_sizes:
-                    for Model in model_list:
-                    #get results of model trained on this set of variables
-                    
-                        md=model.model(subset1,violent_variables,model=Model,pred_classes=class_counts,stand=stand,norm=norm,ytype="binary", target="sentence_length",train_size=train_size)
-    
-                        md.train()
-                        md.predict()
-                        md.get_scores()
-                        #add results to dictionary
-                        results[count]={}
-                        results[count]['model']=Model
-                        results[count]['classes']=class_counts
-                        results[count]['Stand']=stand
-                        results[count]['Norm']=norm
-                        results[count]['Train size']=train_size
-                        count=+1
-                        print(count,Model,train_size,class_counts,norm,stand)
-
-'''
 
 
 
--- src/var_processor.py
+++ src/var_processor.py
@@ -111,7 +111,7 @@ class var_processor:
         self.target=target
         self.norm=norm
         self.colname = colname
-        #print(colname)
+        print(colname)
         self.col=dataset[colname]
         self.dataset = dataset
         self.config=config
@@ -119,28 +119,41 @@ class var_processor:
         self.treatment=config['treatment'][colname]
         #print(self.treatment)
         #empty nan list to populate
-        self.nans=[]
+        self.dkref=[]
+        self.missing=[]
         self.enc=OneHotEncoder(sparse=False)
         self.output_categories=[]        
 
-        self.get_nans()
+        self.get_dkref()
+        self.get_missing()
         self.process()
 
         
-    def get_nans(self):
+    def get_dkref(self):
         try:
-            self.nans=list(literal_eval(self.config['dk_ref_miss'][self.colname]))
+            self.dkref=list(literal_eval(self.config['dk_ref'][self.colname]))
         except:
-            single_nan=literal_eval(self.config['dk_ref_miss'][self.colname])
-            self.nans.append(single_nan)
+            single_dkref=literal_eval(self.config['dk_ref'][self.colname])
+            self.dkref.append(single_dkref)
+            #self.dkref=single_dkref
+
+    def get_missing(self):
+        try:
+            self.missing=list(literal_eval(self.config['missing'][self.colname]))
+        except:
+            single_missing=literal_eval(self.config['missing'][self.colname])
+            self.missing.append(single_missing)
 
     def process(self):
         #print('Treatment is {}'.format(self.treatment))
         if self.treatment=='one_hot':
             if self.colname=='V0772':
+                print('States being processed *****')
                 self.prep_nans_states()
-                
-            self.prep_nans_one_hot()
+            
+            else:
+                self.prep_nans_one_hot()
+            
             self.one_hot_encode()
             
         elif self.treatment=='cont_wnans':
@@ -152,78 +165,21 @@ class var_processor:
     def min_max_scaling(self):
         series=self.output_col
         return (series - series.min()) / (series.max() - series.min())
-  
-    def yprocess_cont_wnans(self):
- 
-        ####1) Replacing skips (-8)
-        #missing values to be replaced with a 0 if skip is allowable
-        #we know skip is allowable if -8 is not in nans
-        #for example, if we expect that a question is required in the subset under analysis
-        if '-8' not in self.nans:
-            self.col_nans_processed=self.col.replace(np.nan,0)
-        else:
-            #print(self.colname)
-            self.col_nans_processed=self.col
-        
-        ####2) Replacing np.nan values
-        #nans that are not skips should be replaced intelligently
-        #replaced with most frequent value        
-        #if skips are not allowed, then -8 will be on the nan list and will also be replaced
-        if np.nan in self.col_nans_processed.values:
-            warnings.warn('np.nan values found and replaced with most common value')
-            mc=self.col_nans_processed.value_counts(dropna=False).idxmax()
-            self.output_col=self.col_nans_processed.replace(self.nans, mc)
-        else:
-            self.output_col=self.col_nans_processed
-
-        #update name of pandas series so that the variable name propogates through to output df    
-        self.output_col.name=self.colname
-        
-        ####3) Replace nans listed in config file with a 0
-        #can't be left as np.nan because will cause errors
-        self.output_col.replace(self.nans, 0,inplace=True)
-        
-        ####4) Standardise values
-        #need to check if this is one of the target variables before standardising
-        if self.target=='sentence_length':
-            if self.colname not in ['V0402','V0403','V0404','V0413','V0414','V0415']:
-
-                
-                if self.stand==1:
-                    self.output_col=(self.output_col-self.output_col.mean())/self.output_col.std()
-                else:
-                    self.output_col=self.col_nans_processed
-                
-                ###4) Normalise values
-                if self.norm==1:
-                    self.output_col = self.min_max_scaling()
-            
-
 
     def process_cont_wnans(self):
         
-        
-        #-8 means the logic skipped it, so it should be replaced with zero
-        #the other nans should have information, so we should be replacing those values intelligently
-        self.col_nans_processed=copy.deepcopy(self.col.replace(-8,0))
+        #-replace missing values defined in config value with zero
+        self.col_nans_processed=copy.deepcopy(self.col.replace(self.missing,0))
         #print('During nan processing')
         #print(sorted(self.col_nans_processed.index)[0:10])
         #instead of -8, skips sometimes have np.nan so we want to make these zeroes
         self.col_nans_processed=self.col_nans_processed.replace(np.nan,0)
         #print(sorted(self.col_nans_processed.index)[0:10])
         
-        #then we need to identify the nan values for this column and replace them with np.nan
-        #this allows the pandas fill na class to be used
-        self.col_nans_processed=self.col_nans_processed.replace(self.nans,np.nan)
-        #print(sorted(self.col_nans_processed.index)[0:10])
-        
-        #then use the pandas interpolate function
-        #issue with this is that it produces negative numbers and also produces decimal numbers
-        #only positive intergers are relevant
-        #self.col_nans_processed=self.col_nans_processed.interpolate(method='polynomial',order=2)
-        
+        #then we need to identify the dk_ref values for this column and replace them with meaningful values
+        #tried replacing with na and using fill na, but negative numbers caused issues
         #try replacing with mean instead
-        self.col_nans_processed=self.col_nans_processed.replace(np.nan, round(self.col_nans_processed.mean()))
+        self.col_nans_processed=self.col_nans_processed.replace(self.dkref, round(self.col_nans_processed.mean()))
         #print(sorted(self.col_nans_processed.index)[0:10])
             
         self.output_col=copy.deepcopy(self.col_nans_processed)
@@ -233,10 +189,17 @@ class var_processor:
         self.output_col.name=self.colname
 
     def prep_nans_states(self):
-        self.col.nans_processed=self.col.replace([-1,-2,np.nan],[self.col.mode,self.col.mode(),self.col.mode()])
-    
+        #replace DK/REF with most common state
+        print('Value counts for state')
+        print(self.col.value_counts(dropna=False))
+        #need to have -1 and -2 as strings
+        self.col_nans_processed=self.col.replace(['-1','-2',np.nan],[self.col.mode(),self.col.mode(),self.col.mode()])
+        print(self.col_nans_processed.value_counts(dropna=False))
+        for ind,val in self.col_nans_processed.iteritems():
+            if type(val)!=str:
+                print(type(val))
+            
     def prep_nans_one_hot(self):
-        
         """
         ####1) Replacing np.nan
         #If skips are allowed (-8,-99 EXcluded from nan list), replace np.nan with -8 (representing a skip)
@@ -250,48 +213,17 @@ class var_processor:
 
 
         """
-        ###1) Replacing missing and np.nan values
-        #If skips are allowed (-8,-99 EXcluded from nan list), replace np.nan with -8 (representing a skip)
+        ###1) Replacing missing, blank and np.nan values
+        #for all the ones that are skipped or empty data put in a -8
         #They will get their own category when encoded
         #Empty strings are converted to np.nan so will be included in this
+        self.col_nans_processed=self.col.replace(np.nan,-8)
+        self.col_nans_processed.replace(self.missing,-8,inplace=True)
         
-        
-        if '-8' not in str(self.nans):
-            self.col_nans_processed=self.col.replace(np.nan,-8)
-        else:
-            self.col_nans_processed=self.col
-        
-        if '99' not in str(self.nans):
-            self.col_nans_processed.replace(99,-8,inplace=True)
-        else:
-            self.col_nans_processed=self.col
-            
-        '''
-        values=self.col_nans_processed.values
-
-        ###2) Check for strings
-        #the encoder needs the values to be the same datatype
-        #this checks that 
-        #should only happen for states V0772
-        #replace with string to match other input
-        if any(isinstance(v,str) for v in values):
-            print([v for v in values if type(v) != str])
-            self.col_nans_processed.replace(to_replace, 'nan',inplace=True)
-            print('any', self.colname)
-         else:
-        '''
-        
-        ####3) Replace nan values defined in config file
-        #then replace the values given as nans for this variable with as na/empty value
-        #they will then be given their own category by the encoder
-        #if -8 is nan (i.e. there should be no skip possible on the question) it will be in the nan list
-        
-        #CHANGE APPROACH: nan values all goe to -8
-            
-        self.col_nans_processed.replace(self.nans, -8,inplace=True)
-    
-
-        
+        #for dk and ref value as defined in the file, replace these with a meaningful value
+        #pandas is asking for replace list to be the same length as list of values to replace
+        replacements=[self.col.mode() for val in self.dkref]
+        self.col_nans_processed.replace(self.dkref,replacements,inplace=True)
 
     
     def one_hot_encode(self):
